a3127ece8b3437bd8d47cb72d7d5df83ff65059c
melonybox <grandmadcow@gmail.com>
2019-11-18 19:03:19 -0500

    Initial setup done, hello world with reducer works.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducer from './reducers/reducer';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    ));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  ,document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
