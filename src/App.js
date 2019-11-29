2575df4acff5f25fefb9ab0ec51c083378e2c9b3
melonybox <grandmadcow@gmail.com>
2019-11-28 22:27:19 -0500

    Initial scale now works correctly after many experiments.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   state = {
     click: false,
     imgWidth: 0,
-    imgHeight: 0
+    imgHeight: 0,
+    defaultScale: 0
   }
 
   componentDidMount = () => {
@@ -37,9 +38,12 @@ class App extends Component {
   handleLoad = (e) => {
     let width = e.target.width
     let height = e.target.height
+    let defaultScale = e.target.parentElement.clientHeight / e.target.height
+
     this.setState((state, props) => ({
       imgWidth: state.imgWidth = width,
-      imgHeight: state.imgHeight = height
+      imgHeight: state.imgHeight = height,
+      defaultScale: state.defaultScale = defaultScale
     }))
   }
 
@@ -55,7 +59,7 @@ class App extends Component {
           </p>
           </div>
           <div className="mapContainer">
-            <img src={`/maps/${this.props.mapName}.jpg`} onLoad={this.handleLoad} alt={this.props.mapName} useMap="#image-map" onMouseDown={this.handleClickDown} onMouseUp={this.handleClickUp} onMouseMove={this.handleMouseMove}>
+            <img src={`/maps/${this.props.mapName}.jpg`} onLoad={this.handleLoad} style={{transform: `scale(${this.state.defaultScale}) translateX(0px) translateY(0px)`}}alt={this.props.mapName} useMap="#image-map" onMouseDown={this.handleClickDown} onMouseUp={this.handleClickUp} onMouseMove={this.handleMouseMove}>
             </img>
             <map name="image-map">
               <area style={{cursor: "pointer"}} alt="a" title="a" coords="947,450,922,425" shape="rect" />
