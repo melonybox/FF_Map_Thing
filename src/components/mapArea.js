a5aa693ac33c56cce115c2209aebf1a9f82a2215
melonybox <grandmadcow@gmail.com>
2020-02-25 18:52:18 -0500

    spawned mark size doubled.

--- src/components/mapArea.js
+++ src/components/mapArea.js
@@ -24,7 +24,7 @@ class MapArea extends React.PureComponent {
   render(){
     return(
       <>
-        <area onClick={this.handleSvgClick.bind(this, this.props.elPos)} style={{cursor: "pointer", userSelect: "none"}} alt={this.props.titleName} title={this.props.pointName} coords={`${this.props.xAxis}, ${this.props.yAxis}, 16`} shape="circle" />
+        <area onClick={this.handleSvgClick.bind(this, this.props.elPos)} style={{cursor: "pointer", userSelect: "none"}} alt={this.props.titleName} title={this.props.pointName} coords={`${this.props.xAxis}, ${this.props.yAxis}, ${this.props.markType === "markSpawn" ? "32" : "16"}`} shape="circle" />
       </>
     )
   }
--- src/components/mapSvgPoints.js
+++ src/components/mapSvgPoints.js
@@ -5,7 +5,7 @@ class MapSvgPoints extends React.PureComponent {
   render(){
     return(
       <>
-        <circle cx={this.props.xAxis} cy={this.props.yAxis} r="16" className={this.props.markType} />
+        <circle cx={this.props.xAxis} cy={this.props.yAxis} r={this.props.markType === "markSpawn" ? "32" : "16"} className={this.props.markType} />
       </>
     )
   }
